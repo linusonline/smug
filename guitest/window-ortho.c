d054d0a41fd08633afc61a234410ff1a29b36059
Linus Lindholm <linusonline@gmail.com>
2012-09-28 10:07:51 +0200

    Add a windowed gui test using gl coordinate system calls.

---
+++ guitest/window-ortho.c
@@ -0,0 +1,68 @@
+#include <GL/glfw.h>
+#include <stdlib.h>
+
+void GLFWCALL windowResize(int width, int height)
+{
+    glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, (GLdouble)width, 0.0, (GLdouble)height);
+}
+
+void initGlWindow()
+{
+    // int glfwInit( void )
+    if (glfwInit() != GL_TRUE)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    // int glfwOpenWindow( int width, int height,
+    //      int redbits, int greenbits, int bluebits,
+    //      int alphabits, int depthbits, int stencilbits,
+    //      int mode )
+    if (glfwOpenWindow(0, 0, 0, 0, 0, 0, 0, 0, GLFW_WINDOW) != GL_TRUE)
+    {
+        // void glfwTerminate( void )
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwSetWindowSizeCallback(windowResize);
+    glClearColor(0.5, 0.0, 0.5, 0.0);
+}
+
+void runMainLoop()
+{
+    BOOL running = TRUE;
+    while (running)
+    {
+        // OpenGL rendering goes here...
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap front and back rendering buffers
+        glfwSwapBuffers();
+
+        // Check if ESC key was pressed or window was closed
+        running = !glfwGetKey(GLFW_KEY_ESC) && glfwGetWindowParam(GLFW_OPENED);
+    }
+}
+
+int main()
+{
+    initGlWindow();
+    runMainLoop();
+    glfwTerminate();
+    return 0;
+}
+
+// void drawStuff()
+// {
+    // glVertexPointer(2, GL_FLOAT, 0, vertexData);
+    // // if (use_texture)
+    // // {
+        // // glTexCoordPointer( 2, GL_FLOAT, 0, currentBatch->data->textureData );
+    // // }
+    // glColorPointer(4, GL_FLOAT, 0, colorData);
+
+    // glDrawArrays(GL_QUADS, 0, batchSize);
+// }
