e9f3c2a8e7b358b096e6e4b8af058ee381b61d08
Linus Lindholm <linusonline@gmail.com>
2012-09-28 10:07:28 +0200

    Clean up gui tests.

--- guitest/fullscreen.c
+++ guitest/fullscreen.c
@@ -1,35 +1,47 @@
 #include <GL/glfw.h>
+#include <stdlib.h>
 
-int main()
+void initGlWindow()
 {
     // int glfwInit( void )
     if (glfwInit() != GL_TRUE)
     {
-        return 1;
+        exit(EXIT_FAILURE);
     }
 
     // int glfwOpenWindow( int width, int height,
     //      int redbits, int greenbits, int bluebits,
     //      int alphabits, int depthbits, int stencilbits,
     //      int mode )
-    if (glfwOpenWindow(640, 480, 8, 8, 8, 0, 0, 0, GLFW_FULLSCREEN) != GL_TRUE)
+    if (glfwOpenWindow(0, 0, 0, 0, 0, 0, 0, 0, GLFW_FULLSCREEN) != GL_TRUE)
     {
         // void glfwTerminate( void )
         glfwTerminate();
-        return 2;
+        exit(EXIT_FAILURE);
     }
+    glClearColor(0.5, 0.0, 0.5, 0.0);
+}
+
+void runMainLoop()
+{
     BOOL running = TRUE;
     while (running)
     {
         // OpenGL rendering goes here...
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Swap front and back rendering buffers
+        // Swap front and back rendering buffers. Implicitly calls glfwPollEvents to poll keys and other events.
         glfwSwapBuffers();
 
         // Check if ESC key was pressed or window was closed
         running = !glfwGetKey(GLFW_KEY_ESC) && glfwGetWindowParam(GLFW_OPENED);
     }
+}
+
+int main()
+{
+    initGlWindow();
+    runMainLoop();
     glfwTerminate();
     return 0;
 }
