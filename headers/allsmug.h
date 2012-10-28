4d13b3b599124186633aa059318afab91c6f01d4
Linus Lindholm <linusonline@gmail.com>
2012-10-28 16:31:27 +0100

    Collect all Smug headers into one for convenience.

---
+++ headers/allsmug.h
@@ -0,0 +1,39 @@
+#ifndef SMUG_ALLSMUG_H
+#define SMUG_ALLSMUG_H
+
+#include <common.h>
+
+#include <engine/body.h>
+#include <engine/collisiondetector.h>
+#include <engine/engine.h>
+#include <engine/gameobject.h>
+#include <engine/mainloop.h>
+
+#include <graphics/camera.h>
+#include <graphics/drawable.h>
+#include <graphics/graphics.h>
+#include <graphics/image.h>
+#include <graphics/renderbatch.h>
+#include <graphics/renderqueue.h>
+#include <graphics/sprite.h>
+#include <graphics/spriteanimation.h>
+#include <graphics/spritesheet.h>
+#include <graphics/texture.h>
+
+#include <input/controller.h>
+#include <input/input.h>
+
+#include <platform/file.h>
+
+#include <utils/binarytree.h>
+#include <utils/console.h>
+#include <utils/hook.h>
+#include <utils/linkedlist.h>
+#include <utils/log.h>
+#include <utils/map.h>
+#include <utils/null_console.h>
+#include <utils/stdout_console.h>
+#include <utils/string_console.h>
+#include <utils/rectangle.h>
+
+#endif /* SMUG_ALLSMUG_H */
